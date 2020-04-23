0461d1deb0ed3385007b7128e1fe5cf5d07ba94c
Pawan Aichra <aichrapawan@gmail.com>
2020-04-24 01:59:14 +0530

    hub connection issue resolved

--- js/connect.js
+++ js/connect.js
@@ -4,8 +4,8 @@ function ab2str(buf) {
   	return decoder.decode(new Uint8Array(buf));
 }
 function str2ab(str) {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
+  var buf = new ArrayBuffer(str.length); // 2 bytes for each char
+  var bufView = new Uint8Array(buf);
   for (var i=0, strLen=str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
   }
@@ -20,19 +20,26 @@ function connect(){
 	});
 }
 
+function proceed(data){
+	sendHub("Supports UserCommand NoGetINFO NoHello UserIP2 TTHSearch ZPipe0");
+	  sendHub("Key "+lockMsgToKey(ab2str(data)));
+	  sendHub("ValidateNick temp");
+	  sendHub("MyPass temp");
+	  sendHub("Version 1,0091");
+	  sendHub("GetNickList");
+	  sendHub("MyINFO $ALL temp <++ V:0.867,M:A,H:0/1/0,S:3>$ $1000$$0$");
+}
+
 function onConnectedCallback(resultCode){
 	if(resultCode>0){
 		console.log("connected");
 	}
 	chrome.sockets.tcp.onReceive.addListener(function(info) {
 	  console.log(ab2str(info.data));
-	  sendHub("Supports UserCommand NoGetINFO NoHello UserIP2 TTHSearch ZPipe0");
-	  sendHub("Key "+lockMsgToKey(ab2str(info.data)));
-	  sendHub("ValidateNick temp");
-	  sendHub("MyPass temp");
-	  sendHub("Version 1,0091");
-	  sendHub("GetNickList");
-	  sendHub("MyINFO $ALL temp <++ V:0.867,M:A,H:0/1/0,S:3>$ $1000$$0$");
+	  var result=ab2str(info.data).split(" ");
+	  console.log(result[0]);
+	  if(result[0]=="$Lock")
+	  	proceed(info.data);
 	  // sendHub("ConnectToMe 98765432 10.147.231.130:58060");
 	});
 }
@@ -50,8 +57,8 @@ function sendHub(msg){
 function lockMsgToKey(lockMsg){
 	lockMsg = lockMsg.split(" ");
 	var buffer = str2ab(lockMsg[1]);
-	var lock = new Int16Array(buffer);
-	var key = new Int16Array(lock.length);
+	var lock = new Uint8Array(buffer);
+	var key = new Uint8Array(lock.length);
 	for(var i=1;i<lock.length;i++){
 		key[i] = lock[i] ^ lock[i-1];
 	}
@@ -62,7 +69,7 @@ function lockMsgToKey(lockMsg){
 	var result = new Array();
 	for(const k of key){
 		if(k==0 || k==5){
-			const temp = new Int16Array(str2ab("/%DCN00"+k+"%/"));
+			const temp = new Uint8Array(str2ab("/%DCN00"+k+"%/"));
 			z = temp.entries();
             x = z.next();
 			while(!x.done){
@@ -71,7 +78,7 @@ function lockMsgToKey(lockMsg){
 			}
 		}
 		else if(k==36||k==96){
-			var temp = new Int16Array(str2ab("/%DCN0"+k+"%/"));
+			var temp = new Uint8Array(str2ab("/%DCN0"+k+"%/"));
 			z = temp.entries();
             x = z.next();
 			while(!x.done){
@@ -80,7 +87,7 @@ function lockMsgToKey(lockMsg){
 			}
 		}
 		else if(k==124||k==126){
-			var temp = new Int16Array(str2ab("/%DCN"+k+"%/"));
+			var temp = new Uint8Array(str2ab("/%DCN"+k+"%/"));
 			z = temp.entries();
             x = z.next();
 			while(!x.done){
@@ -91,9 +98,12 @@ function lockMsgToKey(lockMsg){
 		else
 			result.push((k));
 	}
-	var r = Int16Array.from(result);
+	var r = Uint8Array.from(result);
 	console.log(r);
-	return ab2str(r);
+	var decoder = new TextDecoder("utf-8");
+	var res = decoder.decode(r);
+	console.log(res);
+	return res;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
